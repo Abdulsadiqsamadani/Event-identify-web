5eaa958701bab45e939bc3cd562fc2074a48340b
samadani <blerckmermberh@gmail.com>
2026-02-26 21:57:45 +0100

    mobile version is almost fixed including js

--- js/main.js
+++ js/main.js
@@ -16,35 +16,55 @@ document.addEventListener('DOMContentLoaded', () => {
       name: "Dr shehu S-Tudu",
       role: "AI Engineer",
       description: "Dr shehu S Tudu is a senior lecturer in sokoto state university and software development expert building innovative and ethical solutions for Africa. He focus in creating scalable applications that solve real world problems while empowering local tech commuinities.",
+      image: "Asset/sheu.jpg",
     },
     {
       name: "Sahalu Aminu KOKO",
       role: "Software Developer",
       description: "Sahalu is a fullstack developer experienced in building scalable web applications. He is skilled in both frontend and backend technologoes and enjoys mentoring emerging developers in practical coding solution.",
+      image: "Asset/sahalu.JPG",
     },
     {
       name: "Aliyu Dahiru",
       role: "Cyber secuirity Expert",
       description: "Mal Aliyu is a cybersecurity expert with over 10 years of experience in securing digital infrastructures across Africa, mal ALiyu dahiru specialize in securing infratructure and protecting orgniazation against cyber threats. He trains young developers on best secuirity practices and ethical hacking to build a safer online ecosystem",
+      image: "Asset/aliyu.jpg",
     },
     {
       name: "Sadiq Abdulbasid Abdussamad",
       role: "software developer",
       description: "host",
+      image: "Asset/sadiq.jpg",
     },
   ];
   
 
   const container = document.getElementById('speakers-container');
+  const toggleBtn = document.getElementById("toggle-btn")
 
-  speakers.forEach((speaker) => {
-    const card = document.createElement('div');
+  let showAll = false;
+
+  function displaySpeakers() {
+    container.innerHTML = "";
+    const speakersToshow = showAll ? speakers : speakers.slice(0, 2);
+    speakersToshow.forEach((speaker) => {
+      const card = document.createElement("div");
+      card.classList.add("speaker-card");
+     
     card.innerHTML = `
-      <img src="${speaker.image}" al
+     ${speaker.image ? `<img src="${speaker.image}" alt ="${speaker.name}">` :""}
       <h2>${speaker.name}</h2>
       <p><strong>${speaker.role}</strong></p>
       <p>${speaker.description}</p>
+      </div>
     `;
     container.appendChild(card);
-  });
+   });
+  toggleBtn.textContent = showAll ? "Less" : "More";
+}
+toggleBtn.addEventListener("click",() => {
+  showAll = !showAll;
+  displaySpeakers();
+});
+displaySpeakers();
 });
